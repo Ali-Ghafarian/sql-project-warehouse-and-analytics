f769572552c496b0e438f2911ed76fba6dbf06b6
Ali-Ghafarian <113483770+Ali-Ghafarian@users.noreply.github.com>
2025-03-17 15:42:36 -0700

    add comments to database_init.sql

--- database_init.sql
+++ database_init.sql
@@ -1,22 +1,30 @@
 /*
 =============================================================
-Create Database and Schemas
+Database and Schema Setup
 =============================================================
-Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
-    within the database: 'bronze', 'silver', and 'gold'.
-	
-WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
+Purpose:
+    This script creates a new database named 'DataWarehouse'. 
+    If the database is already present, it is first removed and then recreated.
+    In addition, the script establishes three schemas within the database:
+    'bronze', 'silver', and 'gold'.
+
+IMPORTANT:
+    Executing this script will result in the deletion of the entire 'DataWarehouse' database if it exists.
+    All existing data in the database will be lost permanently.
+    Please proceed with caution and ensure you have comprehensive backups before running this script.
 */
 
-USE master;
-GO
+--USE master;
+--GO
 
 -- Drop and recreate the 'DataWarehouse' database
+/*
+Restricts Connections:
+Switching to single-user mode forces all users to disconnect, allowing only one connection to the database. This is important because the DROP DATABASE command requires that no other connections are interfering. If multiple connections are active, the drop operation can fail or produce errors.
+
+Immediate Transaction Rollback:
+The option WITH ROLLBACK IMMEDIATE terminates any running transactions immediately by rolling them back. This prevents open transactions from blocking the operation, ensuring that all locks and running sessions are cleared so the database can be safely dropped.
+*/
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
