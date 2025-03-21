6d8ad86f7f2eaa9aca3f92c17ad342cf886764ca
Ali-Ghafarian <113483770+Ali-Ghafarian@users.noreply.github.com>
2025-03-21 14:21:31 -0700

    add quality check notebook

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -4,7 +4,7 @@ Stored Procedure: Load Silver Layer (Bronze -> Silver)
 ===============================================================================
 Script Purpose:
     This stored procedure performs the ETL (Extract, Transform, Load) process to 
-    populate the 'silver' schema tables from the 'bronze' schema.
+    populate the 'silver' schema tables from the 'bronze' schema, based on insights gained from quality_check notebook file.
 	Actions Performed:
 		- Truncates Silver tables.
 		- Inserts transformed and cleansed data from Bronze into Silver tables.
